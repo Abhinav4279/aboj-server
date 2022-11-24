8bfe9e0b10e596b0b11bc81ee1c2098be0ff56f9
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-11-24 14:42:40 +0530

    db entry

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const { generateFile } = require("./generateFile");
 const { executeCpp } = require("./executeCpp");
 const { executeAsm } = require("./executeAsm")
+const Job = require('./models/Job');
 
 mongoose.connect("mongodb://localhost/compilerapp", {
   useNewUrlParser: true,
@@ -23,6 +24,26 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/status', async(req, res) => {
+  const jobId = req.query.id;
+  if(jobId === undefined) {
+    return res.status(400).json({success: false, error: 'id missing'});
+  }
+
+  try {
+    const job = await Job.findById(jobId);
+    
+    if(job === undefined) {
+      return res.status(404).json({success: false, error: "job id doesn't exist"})
+    }
+
+    return res.status(200).json({success: true, job});
+  } catch(err) {
+    return res.status(400).json({success: false, error: JSON.stringify(err)});
+  }
+
+})
+
 app.post("/run", async (req, res) => {
   const { language = "cpp", code } = req.body;
 
@@ -32,10 +53,19 @@ app.post("/run", async (req, res) => {
     return res.status(400).json({ success: false, error: "Empty code body!" });
   }
 
+  let job;
   try {
     const filepath = await generateFile(language, code);
+    
+    job = await new Job({language, filepath}).save()
+    const jobId = job["_id"];
+    console.log(job)
+
+    res.status(201).json({success: true, jobId});
 
     let output;
+
+    job["startedAt"] = new Date();
     switch(language) {
       case 'cpp':
         output = await executeCpp(filepath);
@@ -44,9 +74,21 @@ app.post("/run", async (req, res) => {
         output = await executeAsm(filepath);
     }
 
-    return res.json({ filepath, output });
+    job["completedAt"] = new Date();
+    job["status"] = "success";
+    job["output"] = output;
+
+    await job.save();
+
+    console.log({file, output});
+    // return res.json({ filepath, output });
   } catch (err) {
-    res.status(500).json({err});
+    job['completedAt'] = new Date();
+    job['status'] = 'error';
+    job['output'] = JSON.stringify(err);
+    await job.save();
+    console.log(err);
+    // res.status(500).json({err});
   }
 });
 
