13d6b893e7e2629fa585555cfc53a7b8c2f34d3e
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-11-24 14:42:52 +0530

    db entry

--- index.js
+++ index.js
@@ -24,26 +24,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/status', async(req, res) => {
-  const jobId = req.query.id;
-  if(jobId === undefined) {
-    return res.status(400).json({success: false, error: 'id missing'});
-  }
-
-  try {
-    const job = await Job.findById(jobId);
-    
-    if(job === undefined) {
-      return res.status(404).json({success: false, error: "job id doesn't exist"})
-    }
-
-    return res.status(200).json({success: true, job});
-  } catch(err) {
-    return res.status(400).json({success: false, error: JSON.stringify(err)});
-  }
-
-})
-
 app.post("/run", async (req, res) => {
   const { language = "cpp", code } = req.body;
 
